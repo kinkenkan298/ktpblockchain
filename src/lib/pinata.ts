61b4b27a73decaf3c2fb5871494c525150676b8d
kinkenkan298 <akin@programmer.net>
2025-11-15 04:12:33 +0700

    feat: install wagmi dan pinata untuk blockchain

---
+++ src/lib/pinata.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import { PinataSDK } from "pinata";
+
+export const pinata = new PinataSDK({
+  pinataJwt: process.env.PINATA_JWT,
+  pinataGateway: process.env.PINATA_GATEWAY,
+});
